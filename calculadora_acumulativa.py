5d8d65d02b350db41b66f2c1627d5a17bdf48825
Naty-ship <bisponatanielly5@gmail.com>
2025-05-13 21:12:44 -0300

    Add files via upload

---
+++ calculadora_acumulativa.py
@@ -0,0 +1,8 @@
+soma = 0
+numero =1
+while numero !=0:
+    numero=int(input("Digite um número( 0 para sair): "))
+    soma = soma + numero
+    if numero !=0:
+        print (f"a soma até o momento é:{soma}")
+print (f"A soma dos numeros digitadosssss é {soma}")        
---
+++ cardapio- lache.py
@@ -0,0 +1,24 @@
+opcao = 0
+while opcao != 5:
+    print ("Cardapio")
+    print ("1- hambúrguer")
+    print("2-  pizza")
+    print ("3- salada")
+    print("4-  refrigerante")
+    print("5-  sair")
+    opcao = int(input("escolha uma opção: "))
+    if opcao == 1:
+         print ("voce escolheu hamburguer.")
+    elif opcao == 2:
+        print("voce escolheu pizza. ")
+    elif opcao == 3:
+        print("voce escolheu salada. ")
+    elif opcao == 4:
+        print("voce escolheu refrigerante. ")
+    elif opcao == 5:
+        print ("saindo do cardapio. ")
+
+    
+ 
+
+    
